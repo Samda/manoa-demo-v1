05154e1cbeb54137e99dd19ecbf3991b38661079
Sokly <sokly@yoolk.com>
2017-05-04 21:58:59 +0700

    fix account detail

--- app/assets/javascripts/accounts/index.js
+++ app/assets/javascripts/accounts/index.js
@@ -1,20 +1,18 @@
-$(document).ready(function() {
-  $('#messenger').on('click', function(e) {
-    e.preventDefault();
-    $('.messenger').addClass('open')
-  })
+$(document).on('click', '#messenger', function(e) {
+  e.preventDefault();
+  $('.messenger').addClass('open')
+})
 
-  $('#btn-close').on('click', function(e) {
-    $('.messenger').removeClass('open')
-  })
+$(document).on('click', '#btn-close', function(e) {
+  $('.messenger').removeClass('open')
+})
 
-  $('#video').on('click', function(e) {
-    e.preventDefault();
-    $('.video').addClass('open')
-  })
+$(document).on('click', '#video', function(e) {
+  e.preventDefault();
+  $('.video').addClass('open')
+})
 
-  $('#close-video').on('click', function(e) {
-    $('.video').removeClass('open')
-  })
+$(document).on('click', '#close-video', function(e) {
+  $('.video').removeClass('open')
 })
 
