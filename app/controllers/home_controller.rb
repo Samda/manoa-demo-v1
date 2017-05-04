101b73048e43f3e9ff3410cd1c3d0069e25fc08e
samda <muy.samda13@gmail.com>
2017-05-04 08:50:29 +0700

    update seed file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < SecureController
 	def index
-		@grids = ToursGrid.new(params[:tours_grid]) do |scope|
+		@params = params[:tours_grid]
+		if @params
+			@days = @params[:by_number_of_day]
+			@nights = @params[:by_number_of_day]
+			@location = @params[:by_country]
+			@hotel = @params[:by_hotel_rate]
+		end
+
+		@grids = ToursGrid.new(@params) do |scope|
 			scope.paginate(:page => params[:page], :per_page => 10)
     end
 	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,49 +21,105 @@ account = Account.where(agency_name: "Angkor Travel").first_or_create(
 					user: user
 				)
 
-tour = Tour.where(name: "Angkor1").first_or_create(
-										hotel_rating: 5,
-										number_of_days: 3,
-										number_of_night: 2,
-										account: account
-										)
+Tour.where(name: "Angkor1").first_or_create(
+	hotel_rating: 5,
+	number_of_days: 3,
+	number_of_night: 2,
+	account: account,
+	description: 'See the ancient city of Cambodia with many temples.'
+)
+
+Tour.where(name: "Angkor and South of Cambodia ").first_or_create(
+	hotel_rating: 2,
+	number_of_days: 4,
+	number_of_night: 4,
+	account: account,
+	description: 'From the ancient city up to the mountains and the beautiful waterfalls in Cambodia.
+	Siem Reap, Kratie, Ranakiri and Mundulkiri.'
+)
+
+Tour.where(name: "Angkor and North of Cambodia").first_or_create(
+	hotel_rating: 1,
+	number_of_days: 7,
+	number_of_night: 7,
+	account: account,
+	description: 'Enjoy the ancient city of Cambodia with many of temples and seek out north of Cambodia.
+	Siem reap, Phnom Penh, Kampot, Sihanouk ville'
+)
 
 user1 = User.where(email: "demo1@manao.com").first_or_create(password: "12345678")
 account1 = Account.where(agency_name: "Bali Travel").first_or_create(
-					bank_detail: "xxx-xxx-xxx-xxx",
-					agency_sms: "012-999-999",
-					whats_app: "angkor-travel",
-					we_chat: "angkor-travel",
-					agency_email: "admin@angkortravel.com",
-					documents: "some documents",
-					quotations: "some qoutations",
-					country: "Indonasia",
-					city: "Bali",
-					user: user1
-				)
-tour1 = Tour.where(name: "Angkor1").first_or_create(
-										hotel_rating: 5,
-										number_of_days: 3,
-										number_of_night: 2,
-										account: account1
-										)
+						bank_detail: "xxx-xxx-xxx-xxx",
+						agency_sms: "012-999-999",
+						whats_app: "bali-travel",
+						we_chat: "bali-travel",
+						agency_email: "admin@bali-travel.com",
+						documents: "some documents",
+						quotations: "some qoutations",
+						country: "Indonesia",
+						city: "Bali",
+						user: user1
+					)
+
+Tour.where(name: "Bali Destination").first_or_create(
+	hotel_rating: 5,
+	number_of_days: 3,
+	number_of_night: 2,
+	account: account1,
+	description: 'Let the good moment of life in Bali.'
+)
+
+Tour.where(name: "Bali and Jungle trek").first_or_create(
+	hotel_rating: 3,
+	number_of_days: 3,
+	number_of_night: 3,
+	account: account1,
+	description: 'Find the best landscape view of Bali from groun up to the hill cross by the river and many waterfalls. Let try  Jungle Trekking in Bali.'
+)
+
+Tour.where(name: "Bali scuba dive").first_or_create(
+	hotel_rating: 2,
+	number_of_days: 3,
+	number_of_night: 3,
+	account: account1,
+	description: 'Choose a better experience in Bali let go underwater.'
+)
+
+Tour.where(name: "KL, Bali and Lankawi experience").first_or_create(
+	hotel_rating: 1,
+	number_of_days: 5,
+	number_of_night: 5,
+	account: account1,
+	description: 'The tour package lead you from City to islands.'
+)
+
 
 user2 = User.where(email: "demo2@manao.com").first_or_create(password: "12345678")
 account2 = Account.where(agency_name: "Lao Travel").first_or_create(
-					bank_detail: "xxx-xxx-xxx-xxx",
-					agency_sms: "012-999-999",
-					whats_app: "angkor-travel",
-					we_chat: "angkor-travel",
-					agency_email: "admin@angkortravel.com",
-					documents: "some documents",
-					quotations: "some qoutations",
-					country: "Lao",
-					city: "Bakse",
-					user: user2
-				)
-tour2 = Tour.where(name: "Angkor1").first_or_create(
-										hotel_rating: 5,
-										number_of_days: 3,
-										number_of_night: 2,
-										account: account2
-										)
+						bank_detail: "xxx-xxx-xxx-xxx",
+						agency_sms: "012-999-999",
+						whats_app: "bakse-travel",
+						we_chat: "bakse-travel",
+						agency_email: "admin@bakse-travel.com",
+						documents: "some documents",
+						quotations: "some qoutations",
+						country: "Lao",
+						city: "Bakse",
+						user: user2
+					)
+
+Tour.where(name: "Bakse Destination").first_or_create(
+	hotel_rating: 5,
+	number_of_days: 3,
+	number_of_night: 2,
+	account: account2,
+	description: "Spend a good moment in Bakse and around Bakse"
+)
+
+Tour.where(name: "The Mekong experiment").first_or_create(
+	hotel_rating: 5,
+	number_of_days: 5,
+	number_of_night: 5,
+	account: account2,
+	description: "Enjoy you journey long mekong and see how is life on Mekong river."
+)
